eaa51d2e8ca3e3e37bab4fda5914a9af9eb2292a
Sophie Akoth <akothsophie9023.gmail.com>
2025-12-03 12:52:51 +0300

    Installation and setup of the project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+ 
+  
+
+  return (
+    <>
+     
+     <div className="min-h-screen flex flex-col items-center justify-center bg-amber-200">
+
+      <div className="bg-black text-white rounded-lg  shadow-lg p-8 max-w-md w-full" >
+      <h1 className="font-bold">Weather App</h1>
+
+      </div>
+        
+      </div>
+     
+      
+    </>
+  )
+}
+
+export default App
